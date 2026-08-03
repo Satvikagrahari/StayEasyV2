ab193b3d7f8788a03c79adbbcef8ec096e7c576d
Satvik Agrahari <agraharisatvik43@gmail.com>
2026-08-04 00:17:01 +0530

    Intial commit Phase1

---
+++ StayEasy.Domain/Entities/Booking.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StayEasy.Domain.Entities
+{
+    internal class Booking
+    {
+    }
+}
---
+++ StayEasy.Infrastructure/Persistence/StayEasyDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StayEasy.Infrastructure.Persistence
+{
+    internal class StayEasyDbContext
+    {
+    }
+}
